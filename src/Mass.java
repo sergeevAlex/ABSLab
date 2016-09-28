4eaff3e3a3b58dfdb7abafd3dc63ce32de6de59a
sergeevAlex <alexsergeev@i.ua>
2016-09-28 18:16:53 +0300

    fixed exception with counter

--- src/Mass.java
+++ src/Mass.java
@@ -19,21 +19,18 @@ public class Mass {
                 value = b.nextInt();
                 mt.setElem(i, j, value);
             }
-        }
-
-
-
+       }
 //        for (int i = 0; i < n; i++) {
 //            for (int j = 0; j < n; j++) {
-//                  int value = (int) (Math.random()*5);
+//                   value = (int) (Math.random()*10+3);
 //                   mt.setElem(i, j, value);
 //            }
 //        }
         printMatrix(mt);
-        double start = System.currentTimeMillis();
+        long start = System.nanoTime();
         double gauss = gauss(mt, n);
-        double finish = System.currentTimeMillis();
-        double time = finish - start;
+        long finish = System.nanoTime();
+        long time = finish - start;
         System.out.println("The time of gauss = " + time);
         System.out.println(gauss);
     }
--- src/com/lehich/Matrix.java
+++ src/com/lehich/Matrix.java
@@ -5,23 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
 public class Matrix {
-    private int[][] matrix;
+    private double[][] matrix;
     public Matrix() {
     }
     //Размер матрицы
     public void setSize(int colrow) {
-        matrix = new int[colrow][colrow];
+        matrix = new double[colrow][colrow];
         // matrix = new List<List<Integer>>();
         // matrix.add(new ArrayList<Integer>(colrow));
     }
     //Задание элемента
-    public void setElem(int row, int col, int value) {
-        matrix[row][col] = value;
+    public void setElem(int row, int col, double value) {
+      matrix[row][col] = value;
+
        // matrix.add(row,value);
     }
     //Получение элемента
-    public int getElem(int row, int col) {
-       return matrix[row][col];
+    public double getElem(int row, int col) {
+      return matrix[row][col];
     }
     public static Matrix createMatrix(int size) {
         Matrix mt = new Matrix();
@@ -30,7 +31,7 @@ public class Matrix {
     }
     //Свап двух строк матрицы
     public static Matrix swapLine(int swopLine, int targetLine, Matrix mt1, int size) {
-        int tmp;
+        double tmp;
         int secondLine = targetLine;
         for (int firstLine = swopLine; swopLine < size; swopLine++) {
             for (int j = 0; j < size; j++) {
@@ -40,9 +41,11 @@ public class Matrix {
             }
         }
         return mt1;
+
+
     }
     public static double gauss(Matrix mt1, int size) {
-        double result = 1;
+        double result = 1.0;
         int count;
         Matrix mt = new Matrix();
         mt.setSize(size);
@@ -60,21 +63,21 @@ public class Matrix {
         }
         for (int i = 0; i < size; i++) {
             //проверка на НЕнулевой i-тый элемент строки
-            if (mt.getElem(i, i) == 0 && i < size-1) {
-                count = i;
-                do {
-                    if(count < size ){count = size;}
-                    mt.getElem(count, 0);
-                    count++;
+          try {
+              if (mt.getElem(i, i) == 0 && i < size - 1) {
+                  count = i;
+                  do {
+                      mt.getElem(count, 0);
+                      count++;
+                  }
+                  while (mt.getElem(count, 0) == 0);
+                  mt = swapLine(i, count, mt, size);
+                  result = -result; // Если была совершена перестановка —
 
-
-                    //Проблема в счетчике count >size - выход за рамки
-                    //if(count > size){count = size;}
-                }
-                while (mt.getElem(count, 0) == 0);
-                mt = swapLine(i, count, mt, size);
-                result = -result; // Если была совершена перестановка —
-            }                 // меняем знак определителя
+              }// меняем знак определителя
+          }
+          catch (Exception ex)
+          {return 0;}
             for (int x = 0; x < size; x++) {
                 for (int y = 0; y < size; y++) {
                     mt2.setElem(x, y, mt.getElem(x, y));
@@ -83,17 +86,20 @@ public class Matrix {
             //зануление i-того столбца
             for (int j = i + 1; j < size; j++) {
                 for (int k = i; k < size; k++) {
-                    int tmp = mt.getElem(j, k) - ((mt.getElem(i, k) * mt2.getElem(j, i)) / mt.getElem(i, i));
+                    double tmp = mt.getElem(j, k) - ((mt.getElem(i, k) * mt2.getElem(j, i)) / mt.getElem(i, i));
                     mt.setElem(j, k, tmp);
                 }
             }
         }
+
+
         //Вычисление определителя
         for (int x = 0; x < size; x++) {
             result = result * mt.getElem(x, x);
         }
         return result;
     }
+
     public int getSize(){
         int a = matrix.length;
         return a;
